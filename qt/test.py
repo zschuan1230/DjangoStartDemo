29505ae39aece62174fed8db3b4fcf67dcffde33
zhaosc123 <zhaoshichuan@ysskj.com>
2019-10-16 14:56:49 +0800

    上传代码
    上传代码

---
+++ qt/test.py
@@ -0,0 +1,19 @@
+import sys
+
+import sys, os
+if hasattr(sys, 'frozen'):
+    os.environ['PATH'] = sys._MEIPASS + ";" + os.environ['PATH']
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from qt.start import *
+
+class MyWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(MyWindow, self).__init__(parent)
+        self.setupUi(self)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    myWin = MyWindow()
+    myWin.show()
+    sys.exit(app.exec_())
